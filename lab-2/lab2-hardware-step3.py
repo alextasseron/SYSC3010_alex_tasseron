f325a5169663b729f5631b98272cfa037fb5257f
alextasseron <alextasseron@cmail.carleton.ca>
2020-10-08 08:49:19 -0400

    lab exam commit

--- lab-2/lab2-hardware-step3.py
+++ lab-2/lab2-hardware-step3.py
@@ -3,10 +3,10 @@ from time import sleep
 
 sense = sense_hat.SenseHat()
 
-red = (255,0,0)
+blue = (0,0,255)
 
 white = (0,0,0)
 
-sense.show_message("ALEXANDER TASSERON", 0.1, red)
+sense.show_message("NORESSAT REDNAXELA", 0.1, blue)
 
 sense.clear()
